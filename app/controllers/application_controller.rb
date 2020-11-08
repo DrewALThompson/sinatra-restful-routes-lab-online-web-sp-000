f9b0510bd37cc56468ee5c189a0a850c5b881536
Drew Thompson <drewallynthompson@gmail.com>
2020-11-08 06:46:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,9 @@ class ApplicationController < Sinatra::Base
     redirect "/recipes/#{@recipe.id}"
   end
   
-  
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.clear
   
 
 end
